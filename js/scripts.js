1fc3effebf0fe40ada751f91a883e4299d810949
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-24 12:10:55 -0800

    BIGFIX findUnit loops break when remainder > 1

--- js/scripts.js
+++ js/scripts.js
@@ -6,44 +6,44 @@ const volSmallestUnit = [1, 1.23, 2.46, 4.93, 7.37, 14.79, 29.57, 59, 78, 118, 2
 
 function findUnit(convertedInput) {
   let unitArray = [];
-  let remainder = 0;
+  let remainder = 1;
   for (let i = volSmallestUnit.length; i > 0; i--) {
-    if (convertedInput >= volSmallestUnit[i]) {
+    if (convertedInput >= volSmallestUnit[i] && remainder > 1) {
       const unitOne = Math.floor(convertedInput / volSmallestUnit[i]) + volUnitArray[i];
       remainder = convertedInput % volSmallestUnit[i];
-      console.log(unitOne);
+      console.log("unit one = " + unitOne);
+      console.log("remainder = " + remainder);
       unitArray.push(unitOne);
-      // break;
-    }
-    for (let j = volSmallestUnit.length; j > 0; j--) {
-      if (remainder < 1) {
-        break;
-      } else if (remainder >= volSmallestUnit[j]) {
-          const unitTwo = Math.floor(remainder / volSmallestUnit[j]) + volUnitArray[j];
-          remainder = remainder % volSmallestUnit[j];
-          console.log(unitTwo);
-          unitArray.push(unitTwo);
-          break;
-      }
-      for (let l = volSmallestUnit.length; l > 0; l--) {
+      for (let j = volSmallestUnit.length; j > 0; j--) {
         if (remainder < 1) {
           break;
-        } else if (remainder >= volSmallestUnit[l]) {
-            const unitThree = Math.floor(remainder / volSmallestUnit[l]) + volUnitArray[l];
-            remainder = remainder % volSmallestUnit[l];
-            console.log(unitThree);
-            unitArray.push(unitThree);
-            break;
+        } else if (remainder >= volSmallestUnit[j]) {
+            const unitTwo = Math.floor(remainder / volSmallestUnit[j]) + volUnitArray[j];
+            remainder = remainder % volSmallestUnit[j];
+            console.log("unit two = " + unitTwo);
+            console.log("remainder = " + remainder);
+            unitArray.push(unitTwo);
+            for (let l = volSmallestUnit.length; l > 0; l--) {
+              if (remainder < 1) {
+                break;
+              } else if (remainder >= volSmallestUnit[l]) {
+                  const unitThree = Math.floor(remainder / volSmallestUnit[l]) + volUnitArray[l];
+                  remainder = remainder % volSmallestUnit[l];
+                  console.log("unit three = " + unitThree);
+                  console.log("remainder = " + remainder);
+                  unitArray.push(unitThree);
+                  for (let k = volSmallestUnit.length; k > 0; k--) {
+                    if (remainder < 1) {
+                      break;
+                    } else if (remainder >= volSmallestUnit[k]) {
+                        const unitFour = Math.floor(remainder / volSmallestUnit[k]) + volUnitArray[k];
+                        remainder = remainder % volSmallestUnit[k];
+                        console.log("unit four = " + unitFour);
+                        console.log("remainder = " + remainder);
+                        unitArray.push(unitFour);
+              }
         }
-        for (let k = volSmallestUnit.length; k > 0; k--) {
-          if (remainder < 1) {
-            break;
-          } else if (remainder >= volSmallestUnit[k]) {
-              const unitFour = Math.floor(remainder / volSmallestUnit[k]) + volUnitArray[k];
-              remainder = remainder % volSmallestUnit[k];
-              console.log(unitFour);
-              unitArray.push(unitFour);
-              break;
+    }
           }
         }
       }
