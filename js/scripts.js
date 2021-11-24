2931989124b5fc7c2bb65c050467762c71931814
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-24 12:37:11 -0800

    WIP change dual arrays to nested array or object

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,51 @@
-const volUnitArray = ["ml", "quarterTsp", "halfTsp", "tsp", "halfTbsp", "tbsp", "fluidOz/twoTbsp", "quarterCup", "thirdCup", "halfCup", "cup", "pint", "quart", "liter", "gallon"];
-const volSmallestUnit = [1, 1.23, 2.46, 4.93, 7.37, 14.79, 29.57, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785]
+// const volUnitArray = ["ml", "quarterTsp", "halfTsp", "tsp", "halfTbsp", "tbsp", "fluidOz/twoTbsp", "quarterCup", "thirdCup", "halfCup", "cup", "pint", "quart", "liter", "gallon"];
+// const volSmallestUnit = [1, 1.23, 2.46, 4.93, 7.37, 14.79, 29.57, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785]
 
-// I think this is working except for the breaks.  I think there needs to be only a break 
-//at the end and in the if remainder < 1 statements.
+const volUnitMlArray = [["ml", 1],["quarterTsp", 1.23],["halfTsp", 2.46],["tsp", 4.93],["halfTbsp", 7.37],["tbsp", 14.79],["fluidOz/twoTbsp", 29.57],["quarterCup", 59],["thirdCup", 78],["halfCup", 118],["cup", 236.6],["pint", 473.2],["quart", 946.3],["liter", 1000],["gallon", 3785]];
 
 function findUnit(convertedInput) {
   let unitArray = [];
   let remainder = 1;
-  for (let i = volSmallestUnit.length; i > 0; i--) {
-    if (convertedInput >= volSmallestUnit[i] && remainder >= 1) {
-      const unitOne = Math.floor(convertedInput / volSmallestUnit[i]) + volUnitArray[i];
-      remainder = convertedInput % volSmallestUnit[i];
+  const arrayLength = arrayLength;
+  for (let i = arrayLength; i > 0; i--) {
+    if (convertedInput >= volUnitMlArray[i][1] && remainder >= 1) {
+      let currentMl = volUnitMlArray[i][1];
+      let currentUnit = volUnitMlArray[i][0];
+      const unitOne = Math.floor(convertedInput / currentMl) + currentUnit;
+      remainder = convertedInput % currentMl;
       console.log("unit one = " + unitOne);
       console.log("remainder = " + remainder);
       unitArray.push(unitOne);
-      for (let j = volSmallestUnit.length; j > 0; j--) {
+      for (let j = arrayLength; j > 0; j--) {
         if (remainder < 1) {
           break;
-        } else if (remainder >= volSmallestUnit[j]) {
-            const unitTwo = Math.floor(remainder / volSmallestUnit[j]) + volUnitArray[j];
-            remainder = remainder % volSmallestUnit[j];
+        } else if (remainder >= volUnitMlArray[j]) {
+            let currentMl = volUnitMlArray[j][1];
+            let currentUnit = volUnitMlArray[j][0];
+            const unitTwo = Math.floor(remainder / currentMl) + currentUnit;
+            remainder = remainder % currentMl;
             console.log("unit two = " + unitTwo);
             console.log("remainder = " + remainder);
             unitArray.push(unitTwo);
-            for (let l = volSmallestUnit.length; l > 0; l--) {
+            for (let l = arrayLength; l > 0; l--) {
               if (remainder < 1) {
                 break;
-              } else if (remainder >= volSmallestUnit[l]) {
-                  const unitThree = Math.floor(remainder / volSmallestUnit[l]) + volUnitArray[l];
-                  remainder = remainder % volSmallestUnit[l];
+              } else if (remainder >= volUnitMlArray[l]) {
+                  let currentMl = volUnitMlArray[l][1];
+                  let currentUnit = volUnitMlArray[l][0];
+                  const unitThree = Math.floor(remainder / currentMl) + currentUnit;
+                  remainder = remainder % currentMl;
                   console.log("unit three = " + unitThree);
                   console.log("remainder = " + remainder);
                   unitArray.push(unitThree);
-                  for (let k = volSmallestUnit.length; k > 0; k--) {
+                  for (let k = arrayLength; k > 0; k--) {
                     if (remainder < 1) {
                       break;
-                    } else if (remainder >= volSmallestUnit[k]) {
-                        const unitFour = Math.floor(remainder / volSmallestUnit[k]) + volUnitArray[k];
-                        remainder = remainder % volSmallestUnit[k];
+                    } else if (remainder >= volUnitMlArray[k]) {
+                        let currentMl = volUnitMlArray[k][1];
+                        let currentUnit = volUnitMlArray[k][0];
+                        const unitFour = Math.floor(remainder / currentMl) + currentUnit;
+                        remainder = remainder % currentMl;
                         console.log("unit four = " + unitFour);
                         console.log("remainder = " + remainder);
                         unitArray.push(unitFour);
@@ -47,28 +55,14 @@ function findUnit(convertedInput) {
           }
         }
       }
+    } else {
+      console.log(unitArray);
+      return unitArray;
     }
   }
 }
 
 function scaler(input, unit, scale) {
-  const convertInput = input * volSmallestUnit[volUnitArray.indexOf(unit)] * scale;
+  const convertInput = input * volUnitMlArray[volUnitMlArray.indexOf(unit)] * scale;
   return findUnit(convertInput);
 }
-
-/*
-input one volume amount and unit
-input scale amount to 2 decimals
-convert original amount to smallest unit
-multiply by scale amount
-convert to best option for units:
-  largest unit that fits, next unit, next unit, fraction of last unit
-
-eventually support: mass units
-multi-unit input options
-different output option:
-  one measurement/best option
-  to desired decimal place
-  
-
-*/
