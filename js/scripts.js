43e9ebda470923726c932d3532eba3e50b14e6bd
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-01 11:10:06 -0800

    add metric vs us functionality

--- js/scripts.js
+++ js/scripts.js
@@ -39,20 +39,51 @@ function smallestUnit(convertedInput, unitArray, smallestUnitArray) {
 }
 
 function scalerLogic(input, unit, scale) {
-  const volSmallestUnit = [0, 1, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785];
-  const volUnitArray = ["zero", "ml", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "liter", "gallon"];
-  const massSmallestUnit = [0, 1, 28.35, 453.59, 1000, 907185, 1000000];
-  const massUnitArray = ["zero", "gram", "ounce", "pound", "kilogram", "ton", "metric ton"];
+  const volSmallestUnit = [0.1, 1, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785];
+  const volUnitArray = ["tenth ml", "ml", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "liter", "gallon"];
+  const massSmallestUnit = [0.1, 1, 28.35, 453.59, 1000, 907185, 1000000];
+  const massUnitArray = ["tenth gram", "gram", "ounce", "pound", "kilogram", "ton", "metric ton"];
+  const volMetricSmallestUnit = [0, 1, 1000];
+  const volMetricUnitArray = ["zero", "ml", "liter"];
+  const massMetricSmallestUnit = [0, 1, 1000, 1000000];
+  const massMetricUnitArray = ["zero", "gram", "kilogram", "metric ton"];
+  const volUsSmallestUnit = [0, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 3785];
+  const volUsUnitArray = ["zero", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "gallon", "ton"];
+  const massUsSmallestUnit = [0, 28.35, 453.59, 907185];
+  const massUsUnitArray = ["zero", "ounce", "pound", "ton"];
   let convertInput = 0;
+  let result = "";
   if (volUnitArray.indexOf(unit) != -1) {
     convertInput = input * volSmallestUnit[volUnitArray.indexOf(unit)] * scale;
-    return smallestUnit(convertInput, volUnitArray, volSmallestUnit);
+    if (convertInput < 1) {
+      return `${input} ${unit}`;
+    } else if (volMetricUnitArray.indexOf(unit) != -1){
+      result = smallestUnit(convertInput, volMetricUnitArray, volMetricSmallestUnit);
+    } else if (volUsUnitArray.indexOf(unit) != -1){
+      result = smallestUnit(convertInput, volUsUnitArray, volUsSmallestUnit);
+    }
+    if (typeof result === 'object') {
+      return result.join(", ");
+    } else {
+      return result;
+    }
   } else if (massUnitArray.indexOf(unit) != -1) {
     convertInput = input * massSmallestUnit[massUnitArray.indexOf(unit)] * scale;
-    return smallestUnit(convertInput, massUnitArray, massSmallestUnit);
+    if (convertInput < 1) {
+      return `${input} + ${unit}`;
+    } else if (massMetricUnitArray.indexOf(unit) != -1){
+      result = smallestUnit(convertInput, massMetricUnitArray, massMetricSmallestUnit);
+    } else if (massUsUnitArray.indexOf(unit) != -1){
+      result = smallestUnit(convertInput, massUsUnitArray, massUsSmallestUnit);
+    }
+    if (typeof result === 'object') {
+      return result.join(", ");
+    } else {
+      return result;
+    }
   } else if (unit === null) {
     return "";
   } else {
-    return input * scale + " " + unit;
+    return (input * scale) + " " + unit;
   }
 }
