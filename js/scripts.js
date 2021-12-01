ff6a9e7866e0f20c2e1220383557d72ad92ec75d
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-30 19:01:53 -0800

    BUGFIX, solve i=0 problem by creating dummy value

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,36 @@
-const volUnitArray = ["ml", "quarterTsp", "halfTsp", "tsp", "tbsp", "quarterCup", "thirdCup", "halfCup", "cup", "pint", "quart", "liter", "gallon"];
-const volSmallestUnit = [1, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785];
+function unitLogic(iterator, remainder, measurementArray, unitArray, smallestUnitArray) {
+  const unit = Math.floor(" " + remainder / smallestUnitArray[iterator]) + " " + unitArray[iterator];
+  remainder = remainder % smallestUnitArray[iterator];
+  measurementArray.push(unit);
+  return remainder;
+}
 
-function findUnit(convertedInput) {
-  let unitArray = [];
-  let remainder = 1;
-  // how could I DRY these loops?
-  // should this top loop be a do while loop so it always runs once?
-  for (let i = volSmallestUnit.length; i > 0; i--) {
-    if (convertedInput >= volSmallestUnit[i] && remainder >= 1) {
-      const unitOne = Math.floor(convertedInput / volSmallestUnit[i]) + volUnitArray[i];
-      remainder = convertedInput % volSmallestUnit[i];
-      console.log("unit one = " + unitOne);
-      console.log("remainder = " + remainder);
-      unitArray.push(unitOne);
-      for (let j = volSmallestUnit.length; j > 0; j--) {
-        if (remainder < 1) {
-          return unitArray;
-        } else if (remainder >= volSmallestUnit[j]) {
-            const unitTwo = Math.floor(remainder / volSmallestUnit[j]) + volUnitArray[j];
-            remainder = remainder % volSmallestUnit[j];
-            console.log("unit two = " + unitTwo);
-            console.log("remainder = " + remainder);
-            unitArray.push(unitTwo);
-            for (let l = volSmallestUnit.length; l > 0; l--) {
-              if (remainder < 1) {
-                return unitArray;
-              } else if (remainder >= volSmallestUnit[l]) {
-                  const unitThree = Math.floor(remainder / volSmallestUnit[l]) + volUnitArray[l];
-                  remainder = remainder % volSmallestUnit[l];
-                  console.log("unit three = " + unitThree);
-                  console.log("remainder = " + remainder);
-                  unitArray.push(unitThree);
-                  for (let k = volSmallestUnit.length; k > 0; k--) {
-                    if (remainder < 1) {
-                      return unitArray;
-                    } else if (remainder >= volSmallestUnit[k]) {
-                        const unitFour = Math.floor(remainder / volSmallestUnit[k]) + volUnitArray[k];
-                        remainder = remainder % volSmallestUnit[k];
-                        console.log("unit four = " + unitFour);
-                        console.log("remainder = " + remainder);
-                        unitArray.push(unitFour);
-                        return unitArray;
 
+function smallestUnit(convertedInput, unitArray, smallestUnitArray) {
+  let measurementArray = [];
+  let remainder = convertedInput;
+  for (let i = smallestUnitArray.length; i > 0; i--) {
+    if (remainder >= smallestUnitArray[i] && remainder >= 1) {
+      remainder = unitLogic(i, remainder, measurementArray, unitArray, smallestUnitArray);
+      for (let j = smallestUnitArray.length; j > 0; j--) {
+        if (remainder < 1 ) {
+          return measurementArray;
+        } else if (remainder >= smallestUnitArray[j]) {
+          remainder = unitLogic(j, remainder, measurementArray, unitArray, smallestUnitArray);
+          for (let l = smallestUnitArray.length; l > 0; l--) {
+            if (remainder < 1) {
+              return measurementArray;
+            } else if (remainder >= smallestUnitArray[l]) {
+              remainder = unitLogic(l, remainder, measurementArray, unitArray, smallestUnitArray);
+              for (let k = smallestUnitArray.length; k > 0; k--) {
+                if (remainder < 1) {
+                  return measurementArray;
+                } else if (remainder >= smallestUnitArray[k]) {
+                  remainder = unitLogic(k, remainder, measurementArray, unitArray, smallestUnitArray);
+                  return measurementArray;
+                }
               }
-        }
-    }
+            }
           }
         }
       }
@@ -52,7 +38,21 @@ function findUnit(convertedInput) {
   }
 }
 
-function scaler(input, unit, scale) {
-  const convertInput = input * volSmallestUnit[volUnitArray.indexOf(unit)] * scale;
-  return findUnit(convertInput);
+function scalerLogic(input, unit, scale) {
+  const volSmallestUnit = [0, 1, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785];
+  const volUnitArray = ["zero", "ml", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "liter", "gallon", "ton"];
+  const massSmallestUnit = [0, 1, 28.35, 453.59, 1000, 907185];
+  const massUnitArray = ["zero", "gram", "ounce", "pound", "kilogram", "ton"];
+  let convertInput = 0;
+  if (volUnitArray.indexOf(unit) != -1) {
+    convertInput = input * volSmallestUnit[volUnitArray.indexOf(unit)] * scale;
+    return smallestUnit(convertInput, volUnitArray, volSmallestUnit);
+  } else if (massUnitArray.indexOf(unit) != -1) {
+    convertInput = input * massSmallestUnit[massUnitArray.indexOf(unit)] * scale;
+    return smallestUnit(convertInput, massUnitArray, massSmallestUnit);
+  } else if (unit === null) {
+    return "";
+  } else {
+    return input * scale + " " + unit;
+  }
 }
