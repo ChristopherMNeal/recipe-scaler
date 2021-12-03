9b7b714189b4de19f9dc420b987072223337f424
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-02 23:31:37 -0800

    BUGFIX: correct handling of vary small convertInput in convertUtility();

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function unitLogic(iterator, remainder, measurementArray, unitArray, smallestUnitArray) {
   const unit = Math.floor(remainder / smallestUnitArray[iterator]) + " " + unitArray[iterator];
-  remainder = (remainder % smallestUnitArray[iterator]).toFixed(2);
+  remainder = (remainder % smallestUnitArray[iterator]);
   measurementArray.push(unit);
   return remainder;
 }
@@ -11,7 +11,6 @@ function smallestUnit(convertedInput, unitArray, smallestUnitArray) {
   let remainder = convertedInput;
   // console.log(" = " + );
   // console.log(" = " + );
-
   for (let i = smallestUnitArray.length; i >= 0; i--) {
     if (remainder >= smallestUnitArray[i] && remainder >= smallestUnitArray[0]) {
       remainder = unitLogic(i, remainder, measurementArray, unitArray, smallestUnitArray);
@@ -42,8 +41,9 @@ function smallestUnit(convertedInput, unitArray, smallestUnitArray) {
 }
 
 function convertUtility(input, unit, scale, measureArray, multiplyArray) {
-  convertInput = input * multiplyArray[measureArray.indexOf(unit)] * scale;
+  let convertInput = input * multiplyArray[measureArray.indexOf(unit)] * scale;
   if (convertInput < multiplyArray[0]) {
+    convertInput = (convertInput/multiplyArray[0]).toFixed(2);
     return `${convertInput} ${measureArray[0]}`;
   } else {
     return smallestUnit(convertInput, measureArray, multiplyArray);
@@ -63,9 +63,9 @@ function scalerLogic(input, unit, scale) {
   const volMetricMeasure = ["ml", "liter"];
   const massMetricMultiply = [1, 1000, 1000000];
   const massMetricMeasure = ["gram", "kilogram", "metric ton"];
-  const volUsMultiply = [0.62, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 3785];
-  const volUsMeasure = ["pinch", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "gallon", "ton"];
-  const massUsMultiply = [3.54, 7.09, 14.17, 28.35, 453.59, 907185];
+  const volUsMultiply = [0.616115, 1.23223, 2.46446, 4.92892, 14.7868, 59.147, 78.8627, 118.294, 236.5881, 473.176, 946.3525, 3785.41, 35239.1];
+  const volUsMeasure = ["pinch", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "gallon", "bushel"];
+  const massUsMultiply = [3.54369, 7.08738, 14.1748, 28.3495, 453.5925, 907185];
   const massUsMeasure = ["eighth ounce", "quarter ounce", "half ounce", "ounce", "pound", "ton"];
   let result = 0;
   if (volMetricMeasure.indexOf(unit) != -1) {
